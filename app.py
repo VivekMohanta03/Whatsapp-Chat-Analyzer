cfb4b59c7ff111f9eca6e91bfacd1d1fac59e282
Vivek Kumar Mohanta <vivek123.mohanta@gmail.com>
2024-03-15 00:32:14 +0530

    Bug fixes

--- app.py
+++ app.py
@@ -134,14 +134,11 @@ if uploaded_file is not None:
         # Emoji Analysis
         st.title("Emoji Analysis: (Top 5)")
         emoji_df = helper.emoji_helper(selected_user, df)
-        if emoji_df == -1:
-            st.subheader('No Emojis Found.')
-        else:
-            col1, col2 = st.columns(2)
-            with col1:
-                st.dataframe(emoji_df)
-            with col2:
-                emoji_df = emoji_df.rename(columns={'Emojis': 0, 'Number of times used': 1})
-                fig, ax = plt.subplots()
-                ax.pie(emoji_df[1].head(), labels=emoji_df[0].head(), autopct="%0.2f")
-                st.pyplot(fig)
+        col1, col2 = st.columns(2)
+        with col1:
+            st.dataframe(emoji_df)
+        with col2:
+            emoji_df = emoji_df.rename(columns={'Emojis': 0, 'Number of times used': 1})
+            fig, ax = plt.subplots()
+            ax.pie(emoji_df[1].head(), labels=emoji_df[0].head(), autopct="%0.2f")
+            st.pyplot(fig)
